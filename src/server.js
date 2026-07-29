f86dcfa11ab7effa97886bce5eec92c1f61c9203
Alexander Ries <alexander@gmail.com>
2026-07-29 16:37:44 -0500

    Create initial Express server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "My Project API is running!"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
